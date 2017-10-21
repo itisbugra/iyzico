405da19c73d96a67dfa7de055b6cdc70e47d1a68
Buğra Ekuklu <ekuklu@icloud.com>
2017-10-21 18:42:37 +0300

    Version bump.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Iyzico.Mixfile do
 
   def project do
     [app: :iyzico,
-     version: "1.5.4",
+     version: "1.5.5",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
