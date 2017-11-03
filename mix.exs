88b79094f794e75cd2efb78fb2bb3ef0e7a749b8
Buğra Ekuklu <ekuklu@icloud.com>
2017-11-03 16:38:16 +0100

    Yay. Release! 🎉

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Iyzico.Mixfile do
 
   def project do
     [app: :iyzico,
-     version: "1.5.6",
+     version: "1.6.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
