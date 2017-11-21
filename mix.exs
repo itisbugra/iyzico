b0294ee25e02afaaa7f307099802f69fa52e50f1
Buğra Ekuklu <ekuklu@icloud.com>
2017-11-21 23:40:15 +0300

    Fixes #10: Missing props stored card retrieval.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Iyzico.Mixfile do
 
   def project do
     [app: :iyzico,
-     version: "1.6.1-rc1",
+     version: "1.6.2",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
